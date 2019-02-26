9448729e4bd35f0050cf70ca6db56f8ff8aab680
adrian gajewski <adigtc001@outlook.com>
2019-02-26 13:12:16 +0100

    Added a Vector class

--- MathLibrary/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace MathLibrary
-{
-    public class Class1
-    {
-    }
-}
---
+++ MathLibrary/Vector2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MathLibrary
+{
+    public class Vector2
+    {
+        public Vector2(float X, float Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        private float x;
+        private float y;
+    }
+}
