7219afad863be2d30687751a9bab13d6094634bf
adrian gajewski <adigtc001@outlook.com>
2019-03-01 14:47:47 +0100

    Added a functions to reduce and increase vector lenght

--- MathLibrary/Vector2.cs
+++ MathLibrary/Vector2.cs
@@ -15,9 +15,35 @@ namespace MathLibrary
         public float x;
         public float y;
 
+        public Vector2 Increase(float s)
+        {
+            Vector2 scaled = new Vector2();
+
+            scaled.x = x * s;
+            scaled.y = y * s;
+
+            return scaled;
+        }
+
+        public Vector2 Reduce(float s)
+        {
+            Vector2 scaled = new Vector2();
+            scaled.x = x / s;
+            scaled.y = y / s;
+
+            return scaled;
+        }
+
         public float Lenght() 
         {
             float lenght = (float)Math.Sqrt((x * x) + (y * y));
+
+            return (float)Math.Round(lenght, 2);
+        }
+
+        public float LengthSqr()
+        {
+            float lenght = (x * x) + (y * y);
 
             return (float)Math.Round(lenght, 2);
         }
--- Program/Program.cs
+++ Program/Program.cs
@@ -11,16 +11,33 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Point point = new Point(0, -1);
+            //  Point point = new Point(0, -1);
 
-            Point point2 = new Point(1, 1);
+            //  Point point2 = new Point(1, 1);
 
-            Vector2 vector = point - point2;
+            //  Point point3 = new Point(2, -1);
 
-            float length = vector.Lenght();
+            //  Vector2 vectorCP = point - point3; //-2, -2
+            //  Vector2 vectorIP = point - point2;
 
-          //  Console.WriteLine("Result: " + vector.x + ", " + vector.y);
-            Console.WriteLine("Result: " + length);
+            //  float lenght_sqr_cp = vectorCP.LengthSqr();
+            //  float length_sqr_ip = vectorIP.LengthSqr();
+
+            ////  Console.WriteLine("Result: " + vector.x + ", " + vector.y);
+            //  Console.WriteLine("Lenght squared of Cp: " + lenght_sqr_cp );
+            //  Console.WriteLine("Lenght squared of Ip: " + length_sqr_ip);
+
+            Vector2 vector = new Vector2(3, 4);
+            Console.WriteLine("Pac man initial speed: " + vector.Lenght());
+
+            Vector2 doubled = vector.Increase(2);
+            Console.WriteLine("Pac man doubled speed: " + doubled.Lenght());
+
+            Vector2 halved = vector.Reduce(2);
+            Console.WriteLine("Pac man halved speed: " + halved.Lenght());
+
+
+
 
         }
     }
